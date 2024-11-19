b086958594d364ca2817cb48225522e58ac4b3a3
KazinHawk <Kalani.Harris@hgs.hiddengeniusproject.org>
2024-11-18 17:19:49 -0800

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,27 @@
-function modelPopUp() {
-    // Your code here.
-}   
+document.addEventListener("DOMContentLoaded", () =>
+{
+
+});
+    const modal = document.getElementById("myModal");
+
+    const openModalButton = document.getElementById("openModal")
+
+    const closeModalButton = document.getElementById("closeModal");
+
+    openModalButton.addEventListener("click", () =>
+        
+    {
+
+    });
+    function CloseModal() {
+        const popup = document.getElementById("myModal");
+
+        popup.style.display = "none";
+    }
+
+    function OpenModal()
+    {
+        const popup = document.getElementById("myModal");
+
+        popup.style.display = "block";
+    }
